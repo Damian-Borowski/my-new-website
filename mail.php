97fdf7ca6e258dede921c4b3534853ccee0d9992
Damian-Borowski <dbcreativeproductions@gmail.com>
2020-10-22 08:34:59 +0200

    My new website under construction

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "dbcreativeproductions@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "<script>alert('Wysłano !'); document.location.href='index.html';</script>";
+?>
